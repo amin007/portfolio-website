f9f45e8941d781a631a84e11fa0030cbe8920995
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-11-07 09:51:14 +0800

    tambah contoh perjanjian

---
+++ docs/perjanjian/perjanjian-berdarah-02.php
@@ -0,0 +1,83 @@
+https://www.docsketch.com/contracts/web-design-contract-template/
+translate inggeris kepada melayu guna google daa
+
+Perjanjian Rekaan Laman Web ("Perjanjian") sedang dibuat di antara [Nama Pelanggan] (itu "Anda") yang terletak di [Alamat Jalan, Bandar, Negeri] dan [Nama Syarikat] (itu "Kami" atau " Bulan DD, 20YY]. [Nama Pelanggan] dan [Nama Syarikat] juga boleh dirujuk sebagai "Parti" atau bersama sebagai "Pihak".
+
+Perkhidmatan Anda ingin laman web dan Kami tidak boleh menunggu untuk membinanya untuk Anda. Kami akan menyediakan Anda dengan perkhidmatan berikut ("Perkhidmatan") dengan cara yang tepat pada masanya dan profesional.
+
+Penerangan Perkhidmatan:
+______________________________________________________________________ ______________________________________________________________________ ______________________________________________________________________ ______________________________________________________________________ ______________________________________________________________________
+
+Jika Anda memutuskan Perkhidmatan tidak betul-betul Apa yang Anda cari, jangan risau kerana Kami boleh mengubah skop Perkhidmatan untuk Anda pada bila-bila masa. Untuk mengemas kini skop Perkhidmatan, semua Yang perlu anda lakukan adalah membiarkan Kami mengetahui apa yang Anda ingin ubah dengan mengisi borang yang mudah yang disebut "Tukar Pesanan". Perintah Perubahan akan menjelaskan apa yang Anda mahu Kami lakukan dengan cara yang berbeza. Sekiranya Perkhidmatan baru memerlukan lebih banyak kerja untuk Kami, kedua-dua Pihak perlu bersetuju secara bertulis dengan kos yang dikemas kini untuk Perkhidmatan. Bercakap tentang kos ...
+Kos & Bayaran
+Mereka mengatakan tidak ada yang baik dalam hidup, dan ini tidak terkecuali.
+
+Jumlah Kos Perkhidmatan: ___________________________
+Jumlah yang Dibayar di Tandatangan: ___________________________
+Jumlah terhutang pada selesai : ___________________________
+Garis masa
+Kami tahu Anda teruja untuk menerima produk akhir anda, dan Kami tidak mahu meninggalkan Anda menunggu. Kami akan melancarkan lengan baju kami dan mendapatkan kerja pada tarikh yang dipersetujui dan berterusan sehingga memuaskan Perkhidmatan. Ini bermakna bahawa Kami tidak akan membuang tuala dalam sehingga laman web Kami membangunkan untuk Anda melakukan persis seperti yang dipersetujui. Inilah jadual yang akan kita ikuti:
+
+Reka Bentuk Permulaan: ________________________________
+Tinjauan Anda: ________________________________
+
+Kami ingin memaklumkan kepada anda tentang kemajuan yang Kami buat di laman web anda. Kami akan berkongsi kerja kami dengan Anda melalui Google Docs, Dropbox, atau perkhidmatan perkongsian fail lain dan boleh didapati untuk menjawab sebarang soalan yang mungkin anda miliki pada masa itu.
+
+Tinjauan Akhir Anda: ________________________________
+
+Kajian FINAL anda mesti memasukkan sebarang dan semua maklum balas yang anda miliki.
+
+Penyelesaian: ________________________________
+
+Kami akan tersedia untuk [X hari / minggu / bulan] untuk memberi Anda sokongan teknikal yang munasabah dan membetulkan apa-apa kesilapan atau kekurangan yang mungkin.
+Notis Hakcipta
+Notis hak cipta yang menyatakan "© [XXX]" akan dipaparkan di bahagian bawah setiap halaman laman web anda.
+Harta Intelek
+Anda akan memiliki karya, yang juga dikenali sebagai laman web, yang Kami reka bentuk untuk Anda dan mana-mana visual yang Kami sediakan dengannya. Kami akan mengubah produk kerja kami, termasuk apa-apa fail yang diperlukan, dan Anda akan bertanggungjawab untuk penyimpanan mereka. Kami tidak perlu menyimpan salinan. Anda menjamin bahawa Anda mempunyai hak yang sah untuk semua unsur teks, gambar, dan apa-apa lagi yang Anda berikan kepada Kami dan Anda tidak akan bertanggungjawab untuk sebarang tuntutan pihak ketiga.
+
+Kami akan memiliki apa-apa kerja, idea, penemuan, produk, atau maklumat lain yang boleh hak cipta yang kami buat berhubung dengan Perkhidmatan Kami menyediakan. Kami menjamin bahawa Kami mempunyai hak undang-undang untuk semua elemen yang berkaitan dengan Perkhidmatan Kami menyediakan dan tidak akan memegang Anda bertanggungjawab untuk sebarang tuntutan pihak ketiga.
+Kerahsiaan
+Rahsia anda selamat dengan Kami. Ini termasuk maklumat proprietari anda (perkara seperti rahsia perdagangan, pengetahuan, atau maklumat rahsia lain yang tidak tersedia secara umum). Kami berjanji Kami tidak akan menjual maklumat proprietari anda kepada pihak ketiga, tidak kira betapa mereka menawarkan Kami.
+Tugasan
+Pihak-Pihak tidak boleh menetapkan tanggungjawab yang mereka miliki di bawah Perjanjian ini kepada orang lain melainkan kedua-dua Pihak bersetuju dengan penugasan secara bertulis.
+Penamatan
+Kami sangat tidak suka melihat Engkau pergi. Jika Anda memutuskan Kami bukan cawan kopi (atau wain), Anda boleh mengakhiri Perjanjian ini dengan memberikan Kami notis bertulis sepuluh (10) hari dan membayar Kami untuk Perkhidmatan yang Kami telah selesai.
+
+Jika mana-mana Pihak gagal mematuhi tanggungjawab atau kewajipan mereka di bawah Perjanjian ini, Pihak yang satu lagi boleh menamatkan Perjanjian ini dengan memberi sepuluh (10) hari notis bertulis. Perjanjian ini akan ditamatkan secara automatik apabila kedua-dua Pihak telah melaksanakan semua tanggungjawab mereka di bawah Perjanjian dan semua pembayaran telah dibuat.
+
+Perjanjian ini akan ditamatkan secara automatik apabila kedua-dua Pihak telah melaksanakan semua tanggungjawab mereka di bawah Perjanjian dan semua pembayaran telah dibuat.
+Batasan Liabiliti
+Liabiliti anda kepada Kami hanya untuk kos yang perlu dibayar di bawah Perjanjian ini. Anda tidak akan bertanggungjawab kepada Kami, atau mana-mana pihak ketiga, untuk ganti rugi seperti kehilangan keuntungan, kehilangan simpanan, kerosakan sampingan, kerosakan berbangkit, atau ganti rugi khas.
+Resolusi Pertikaian
+Rundingan: Kami mahu melakukan ini. Sekiranya berlaku pertikaian, pihak-pihak bersetuju untuk bekerja ke arah penyelesaian melalui rundingan yang baik.
+Pengantaraan / Arbitrase: Jika membicarakan perkara itu tidak berjalan dengan lancar, salah satu pihak boleh memulakan pengantaraan atau mengikat arbitrase dalam suatu forum yang dipersetujui oleh Para Pihak.
+Litigasi: Sekiranya litigasi diperlukan, Perjanjian ini akan ditafsirkan berdasarkan undang-undang Negara [Negeri], tanpa mengira apa-apa isu konflik undang-undang yang mungkin timbul. Pihak-pihak bersetuju bahawa pertikaian itu akan diselesaikan di mahkamah yang mempunyai bidang kuasa yang kompeten dalam Negara yang dipersetujui.
+Yuran Peguam: Pihak yang berlaku, atau "pemenang" sebagai bukan peguam yang memanggilnya, akan dapat memulihkan yuran peguamnya dan lain-lain kos yang munasabah untuk pertikaian yang diselesaikan oleh timbang tara atau litigasi yang mengikat.
+Kemerosotan
+Sekiranya mana-mana bahagian Perjanjian ini didapati tidak sah, menyalahi undang-undang, atau tidak boleh dikuatkuasakan, seluruh Perjanjian ini masih boleh dikuatkuasakan.
+Kontrak yang lengkap
+Perjanjian ini meletakkan para Pihak keseluruhan pemahaman tentang Perkhidmatan yang akan dilaksanakan dan apa pun yang dipersetujui Pihak-Pihak dalam hitam dan putih (secara literal). Perjanjian ini menggantikan sebarang komunikasi bertulis atau lisan yang lain antara Pihak-Pihak. Apa-apa perubahan selanjutnya kepada Perjanjian ini mesti dibuat secara bertulis dan ditandatangani oleh kedua-dua Pihak.
+Notis
+Semua notis di bawah perjanjian ini mesti dihantar dengan sama ada e-mel dengan pengesahan penerimaan resit, atau surat siput yang disahkan atau berdaftar dengan resit pulangan yang diminta.
+
+Notis perlu dihantar kepada:
+
+[ Nama Syarikat] [Nama Pelanggan]
+[Nama Pertama Nama Akhir] [Nama Pertama Nama Akhir]
+[Alamat Jalan] [Alamat Jalan]
+[Bandar, Negeri, Kod Zip] [Bandar, Negeri, Kod Zip]
+[Alamat E-mel] [Alamat E-mel]
+Let's Shake Hands
+Ink adalah jabat tangan rasmi perniagaan. Jika Anda bersetuju dengan terma Perjanjian ini, sila tandatangan di bawah. Perjanjian ini akan menjadi berkesan pada [Bulan DD, 20YY].
+
+[Nama Pelanggan]
+Tandatangan: ___________________
+Nama: ___________________
+Tajuk: ___________________
+Tarikh: ___________________
+
+[Nama syarikat]
+Tandatangan: ___________________
+Nama: ___________________
+Tajuk: ___________________
+Tarikh: ___________________
