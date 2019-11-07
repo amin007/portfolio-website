6cc98f58f88c35afcb2703abd376e2ec66f3ea10
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-11-07 09:25:11 +0800

    contoh perjanjian daa

---
+++ docs/perjanjian/perjanjian-berdarah-01.php
@@ -0,0 +1,81 @@
+https://www.pandadoc.com/software-development-agreement-template/
+
+Software Development Agreement
+Document subtitle
+
+This Software Development Agreement (the “Agreement” or “Software Development Agreement”) states the terms and conditions that govern the contractual agreement between [Developer.Company] having his principal place of business at [Developer.Address] , (the “Developer”), and [Client.Company] having its principal place of business at [Client.Address] (the “Client”) who agrees to be bound by this Agreement.
+
+WHEREAS, the Client has conceptualized [QUICK DESCRIPTION OF SOFTWARE] (the “Software”), which is described in further detail on Exhibit A, and the Developer is a contractor with whom the Client has come to an agreement to develop the Software.
+
+NOW, THEREFORE, In consideration of the mutual covenants and promises made by the parties to this Software Development Agreement, the Developer and the Client (individually, each a “Party” and collectively, the “Parties”) covenant and agree as follows:
+
+1. Developer’s duties
+The Client hereby engages the Developer and the Developer hereby agrees to be engaged by the Client to develop the Software in accordance with the specifications attached hereto as Exhibit A (the “Specifications”).
+
+The Developer shall complete the development of the Software according to the milestones described on the form attached hereto as Exhibit B. In accordance with such milestones, the final product shall be delivered to the Client by [Final delivery date] (the “Delivery Date”).
+For a period of [Time frame] after delivery of the final product, the Developer shall provide the Client attention to answer any questions or assist solving any problems with regard to the operation of the Software up to [Number of hours] of hours free of charge and billed to the Client at a rate of [Rate] per hour for any assistance thereafter. The Developer agrees to respond to any reasonable request for assistance made by the Client regarding the Software within [Time frame] of the request.
+Except as expressly provided in this Software Development Agreement, the Client shall not be obligated under this Agreement to provide any other support or assistance to the Developer.
+The Client may terminate this Software Development Agreement at any time upon material breach of the terms herein and failure to cure such a breach within [Time frame] of notification of such a breach.
+The Developer shall provide to the Client after the Delivery Date, a cumulative [Time frame] of training with respect to the operation of the Software if requested by the Client.
+2. Delivery
+The Software shall function in accordance with the Specifications on or before the Delivery Date.
+
+If the Software as delivered does not conform with the Specifications, the Client shall within [Time frame] of the Delivery Date notify the Developer in writing of the ways in which it does not conform with the Specifications. The Developer agrees that upon receiving such notice, it shall make reasonable efforts to correct any non-conformity.
+The Client shall provide to the Developer written notice of its finding that the Software conforms to the Specifications within [Time frame] days of the Delivery Date (the “Acceptance Date”) unless it finds that the Software does not conform to the Specifications as described in Section 2(A) herein.
+3. Compensation
+Compensation. In consideration for the Service, the Client shall pay the Company at the rate of [Rate] per hour (the “Hourly Rate”), with a maximum total fee for all work under this Software Development Agreement of [Maximum total fee]. Fees billed under the Hourly Rate shall be due and payable upon the Developer providing the Client with an invoice. Invoices will be provided for work completed by the developer once every [Pay period].
+
+4. Intellectual property rights in the software
+The Parties acknowledge and agree that the Client will hold all intellectual property rights in the Software including, but not limited to, copyright and trademark rights. The Developer agrees not to claim any such ownership in the Software’s intellectual property at any time prior to or after the completion and delivery of the Software to the Client.
+
+5. Change in specifications
+The Client may request that reasonable changes be made to the Specifications and tasks associated with the implementation of the Specifications. If the Client requests such a change, the Developer will use its best efforts to implement the requested change at no additional expense to the Client and without delaying delivery of the Software. In the event that the proposed change will, in the sole discretion of the Developer, require a delay in the delivery of the Software or would result in additional expense to the Client, then the Client and the Developer shall confer and the Client may either withdraw the proposed change or require the Developer to deliver the Software with the proposed change and subject to the delay and/or additional expense. The Client agrees and acknowledges that the judgment as to if there will be any delay or additional expense shall be made solely by the Developer.
+
+6. Confidentiality
+The Developer shall not disclose to any third party the business of the Client, details regarding the Software, including, without limitation any information regarding the Software’s code, the Specifications, or the Client’s business (the “Confidential Information”), (ii) make copies of any Confidential Information or any content based on the concepts contained within the Confidential Information for personal use or for distribution unless requested to do so by the Client, or (iii) use Confidential Information other than solely for the benefit of the Client.
+
+7. Developer warranties
+The Developer represents and warrants to the Client the following:
+
+Development and delivery of the Software under this Agreement are not in violation of any other agreement that the Developer has with another party.
+The Software will not violate the intellectual property rights of any other party.
+For a period of [Time frame] after the Delivery Date, the Software shall operate according to the Specifications. If the Software malfunctions or in any way does not operate according to the Specifications within that time, then the Developer shall take any reasonably necessary steps to fix the issue and ensure the Software operates according to the Specifications.
+8. Indemnification
+The Developer agrees to indemnify, defend, and protect the Client from and against all lawsuits and costs of every kind pertaining to the software including reasonable legal fees due to the Developer’s infringement of the intellectual rights of any third party.
+
+9. No modification unless in writing
+No modification of this Agreement shall be valid unless in writing and agreed upon by both Parties.
+
+8. Applicable law
+This Software Development Agreement and the interpretation of its terms shall be governed by and construed in accordance with the laws of the State of [State] and subject to the exclusive jurisdiction of the federal and state courts located in [County] , [State].
+
+IN WITNESS WHEREOF, each of the Parties has executed this Software Development Agreement, both Parties by its duly authorized officer, as of the day and year set forth below.
+
+[Developer.Company]
+
+Signature
+
+MM/DD/YYYY
+
+[Developer.FirstName] [Developer.LastName]
+
+[Client.Company]
+
+Signature
+
+MM/DD/YYYY
+
+[Client.FirstName] [Client.LastName]
+
+Exhibit A
+Software specifications
+[FREEFORM PRODUCT FUNCTIONALITY DESCRIPTION]
+
+Exhibit B
+Milestone schedule
+Date	Project milestone
+ 	
+ 	
+ 	
+ 	
+ 	
