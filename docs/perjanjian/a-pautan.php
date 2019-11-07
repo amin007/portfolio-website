762e2e90693d942ab9da23e7fe0fd041bc8d5943
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-11-07 15:11:28 +0800

    tambah pautan yang berkenaan

--- docs/perjanjian/a-pautan.php
+++ docs/perjanjian/a-pautan.php
@@ -1,3 +1,5 @@
 https://www.pandadoc.com/software-development-agreement-template/
 https://www.docsketch.com/contracts/web-design-contract-template/
 https://github.com/php-deal/framework
+http://www.columbia.edu/~mr2651/ecommerce3/1st/assignments/Assignment%203/Web%20Site%20Development%20_Simple_.pdf
+https://www.codementor.io/blog/freelance-web-developer-contract-2uncidz6la
