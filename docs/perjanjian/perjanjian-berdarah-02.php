c0255e8aaa3ecb7a46aaf9a5758780b13423d461
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-11-07 16:12:30 +0800

    susun balik ayat

--- docs/perjanjian/perjanjian-berdarah-02.php
+++ docs/perjanjian/perjanjian-berdarah-02.php
@@ -44,11 +44,11 @@ table.excel tbody td
 
 	<div class="card">
 		<div class="card-header">
-			<a class="card-link" data-toggle="collapse" href="#collapseOne">
+			<a class="card-link" data-toggle="collapse" href="#no01">
 			Apakah yang dinamakan <strong>perjanjian</strong>/<em>agreement </em>
 			</a>
 		</div><!-- / class="card-header" -->
-		<div id="collapseOne" class="collapse" data-parent="#accordion">
+		<div id="no01" class="collapse" data-parent="#accordion">
 		<div class="card-body">
 			<p><span class="font-weight-bold">Definisi:</span>1. persetujuan yg dibuat antara dua pihak atau lebih, permuafakatan: Malaysia telah menandatangani satu ~ per­da­gangan dgn Korea Selatan; ~ jual beli; ~ program persetujuan bertulis antara dua pihak (biasanya pihak pelaksana aktiviti dsb dgn pihak yg mempunyai kuasa utk meluluskan kewangan) bagi melaksanakan sesuatu aktiviti (program dsb) mengikut jumlah peruntukan yg diluluskan; 2. Id wasiat (kitab suci); Kitab P~ Lama Kitab Taurat; Kitab P~ Baru Kitab Injil.
 			<span class="font-weight-bold">(Kamus Dewan Edisi Keempat)</span></p>
@@ -61,23 +61,23 @@ table.excel tbody td
 	</div><!-- / class="card" -->
 	<div class="card">
 		<div class="card-header">
-			<a class="collapsed card-link" data-toggle="collapse" href="#collapseTwo">
+			<a class="collapsed card-link" data-toggle="collapse" href="#no02">
 				Isi Kandungan Penjanjian Berdarah Amin Ledang Nombor 1
 			</a>
 		</div><!-- / class="card-header" -->
-		<div id="collapseTwo" class="collapse show" data-parent="#accordion">
+		<div id="no02" class="collapse show" data-parent="#accordion">
 		<div class="card-body">
 <!-- ////////////////////////////////////////////////////////////////////////////////////// -->
 <ul class="list-group">
 <li class="list-group-item" align="justify">
 Perjanjian Rekaan Laman Web ("Perjanjian") sedang dibuat di antara [Nama Pelanggan]<br>
-(itu "Anda") yang terletak di [Alamat Jalan, Bandar, Negeri] dan [Nama Syarikat] <br>
+(itu "Awak") yang terletak di [Alamat Jalan, Bandar, Negeri] dan [Nama Syarikat] <br>
 (itu "Kami" atau " Bulan DD, 20YY]. [Nama Pelanggan] dan [Nama Syarikat] juga boleh dirujuk<br>
-sebagai "Anda" atau bersama sebagai "Pihak".
+sebagai "Awak" atau bersama sebagai "Pihak".
 </li>
 <li class="list-group-item">1. <span class="font-weight-bold">Perkhidmatan.</span>
-<br>Anda ingin laman web dan Kami tidak boleh menunggu untuk membinanya untuk Anda.<br>
-Kami akan menyediakan Anda dengan perkhidmatan berikut ("Perkhidmatan") dengan cara
+<br>Awak ingin laman web dan Kami tidak boleh menunggu untuk membinanya untuk Awak.<br>
+Kami akan menyediakan Awak dengan perkhidmatan berikut ("Perkhidmatan") dengan cara
 yang tepat pada masanya dan profesional.
 
 <br><span class="font-weight-bold">Penerangan Perkhidmatan:</span>
@@ -89,16 +89,16 @@ yang tepat pada masanya dan profesional.
 <br><br>
 
 <p align="justify">
-Jika Anda memutuskan Perkhidmatan tidak betul-betul Apa yang Anda cari, jangan risau kerana
-Kami boleh mengubah skop Perkhidmatan untuk Anda pada bila-bila masa. Untuk mengemas kini skop
-Perkhidmatan, semua Yang perlu anda lakukan adalah membiarkan Kami mengetahui apa yang Anda
+Jika Awak memutuskan Perkhidmatan tidak betul-betul Apa yang Awak cari, jangan risau kerana
+Kami boleh mengubah skop Perkhidmatan untuk Awak pada bila-bila masa. Untuk mengemas kini skop
+Perkhidmatan, semua Yang perlu Awak lakukan adalah membiarkan Kami mengetahui apa yang Awak
 ingin ubah dengan mengisi borang yang mudah yang disebut "Tukar Pesanan". Perintah Perubahan
-akan menjelaskan apa yang Anda mahu Kami lakukan dengan cara yang berbeza. Sekiranya Perkhidmatan
+akan menjelaskan apa yang Awak mahu Kami lakukan dengan cara yang berbeza. Sekiranya Perkhidmatan
 baru memerlukan lebih banyak kerja untuk Kami, kedua-dua Pihak perlu bersetuju secara bertulis
 dengan kos yang dikemas kini untuk Perkhidmatan. Bercakap tentang kos ...</p>
 
 </li>
-<li class="list-group-item">2. <span class="font-weight-bold">Kos & Bayaran.</span>
+<li class="list-group-item">2. <span class="font-weight-bold">Kos &amp; Bayaran.</span>
 <br>Mereka mengatakan tidak ada yang baik dalam hidup, dan ini tidak terkecuali.
 <br>
 <br><span class="font-weight-bold">Jumlah Kos Perkhidmatan :</span>___________________________
@@ -107,92 +107,145 @@ dengan kos yang dikemas kini untuk Perkhidmatan. Bercakap tentang kos ...</p>
 </li>
 <li class="list-group-item">3. <span class="font-weight-bold">Garis masa.</span>
 <p align="justify">
-Kami tahu Anda teruja untuk menerima produk akhir anda, dan Kami tidak mahu meninggalkan Anda
-menunggu.Kami akan melancarkan lengan baju kami dan mendapatkan kerja pada tarikh yang dipersetujui
-dan berterusan sehingga memuaskan Perkhidmatan. Ini bermakna bahawa Kami tidak akan membuang tuala
-dalam sehingga laman web Kami membangunkan untuk Anda melakukan persis seperti yang dipersetujui.
+Kami tahu Awak teruja untuk menerima produk akhir Awak, dan Kami tidak mahu meninggalkan Awak
+menunggu. Kami akan melipat lengan baju kami dan bekerja pada tarikh yang dipersetujui dan berterusan
+sehingga memuaskan Perkhidmatan. Ini bermakna bahawa Kami tidak akan membuang tuala
+dalam sehingga laman web Kami membangunkan untuk Awak melakukan persis seperti yang dipersetujui.
 Inilah jadual yang akan kita ikuti:</p>
 
-<p><span class="font-weight-bold">Reka Bentuk Permulaan:</span>________________________________</p>
-<p><span class="font-weight-bold">Tinjauan Anda:</span>________________________________</p>
+<p><span class="font-weight-bold">Reka Bentuk Permulaan:</span>________________________________
+<br><span class="font-weight-bold">Tinjauan Awak:</span>________________________________</p>
 
 <p align="justify">
-Kami ingin memaklumkan kepada anda tentang kemajuan yang Kami buat di laman web anda. Kami akan
-berkongsi kerja kami dengan Anda melalui Google Docs, Dropbox, atau perkhidmatan perkongsian fail lain
-dan boleh didapati untuk menjawab sebarang soalan yang mungkin anda miliki pada masa itu.</p>
+Kami ingin memaklumkan kepada Awak tentang kemajuan yang Kami buat di laman web Awak. Kami akan
+berkongsi kerja kami dengan Awak melalui Google Docs, Dropbox, atau perkhidmatan perkongsian fail lain
+dan boleh didapati untuk menjawab sebarang soalan yang mungkin Awak miliki pada masa itu.</p>
 
-<p><span class="font-weight-bold">Tinjauan Akhir Anda:</span>________________________________</p>
+<p><span class="font-weight-bold">Tinjauan Akhir Awak:</span>________________________________</p>
 
-<p align="justify">Kajian FINAL anda mesti memasukkan sebarang dan semua maklum balas yang anda miliki.</p>
+<p align="justify">Kajian FINAL Awak mesti memasukkan sebarang dan semua maklum balas yang Awak miliki.</p>
 
 <p><span class="font-weight-bold">Penyelesaian:</span>________________________________</p>
 
 <p align="justify">
-Kami akan tersedia untuk [X hari / minggu / bulan] untuk memberi Anda sokongan teknikal yang munasabah
+Kami akan tersedia untuk [X hari / minggu / bulan] untuk memberi Awak sokongan teknikal yang munasabah
 dan membetulkan apa-apa kesilapan atau kekurangan yang mungkin.</p>
 
 
 </li>
 <li class="list-group-item">4. <span class="font-weight-bold">Notis Hakcipta.</span>
 <p align="justify">
-Notis hak cipta yang menyatakan "&copy; [XXX]" akan dipaparkan di bahagian bawah setiap halaman laman web anda.</p>
+Notis hak cipta yang menyatakan "&copy; [XXX]" akan dipaparkan di bahagian bawah setiap halaman laman web Awak.</p>
 </li>
 <li class="list-group-item">5. <span class="font-weight-bold">Harta Intelek.</span>
+
 <p align="justify">
-Anda akan memiliki karya, yang juga dikenali sebagai laman web, yang Kami reka bentuk untuk Anda dan mana-mana visual yang Kami sediakan dengannya. Kami akan mengubah produk kerja kami, termasuk apa-apa fail yang diperlukan, dan Anda akan bertanggungjawab untuk penyimpanan mereka. Kami tidak perlu menyimpan salinan. Anda menjamin bahawa Anda mempunyai hak yang sah untuk semua unsur teks, gambar, dan apa-apa lagi yang Anda berikan kepada Kami dan Anda tidak akan bertanggungjawab untuk sebarang tuntutan pihak ketiga.</p>
+Awak akan memiliki karya, yang juga dikenali sebagai laman web, yang Kami reka bentuk untuk
+Awak dan mana-mana visual yang Kami sediakan dengannya. Kami akan mengubah produk kerja kami,
+termasuk apa-apa fail yang diperlukan, dan Awak akan bertanggungjawab untuk penyimpanan mereka.
+Kami tidak perlu menyimpan salinan. Awak menjamin bahawa Awak mempunyai hak yang sah untuk semua
+unsur teks, gambar, dan apa-apa lagi yang Awak berikan kepada Kami dan Awak tidak akan
+bertanggungjawab untuk sebarang tuntutan pihak ketiga.</p>
+
 <p align="justify">
-Kami akan memiliki apa-apa kerja, idea, penemuan, produk, atau maklumat lain yang boleh hak cipta yang kami buat berhubung dengan Perkhidmatan Kami menyediakan. Kami menjamin bahawa Kami mempunyai hak undang-undang untuk semua elemen yang berkaitan dengan Perkhidmatan Kami menyediakan dan tidak akan memegang Anda bertanggungjawab untuk sebarang tuntutan pihak ketiga.</p>
+Kami akan memiliki apa-apa kerja, idea, penemuan, produk, atau maklumat lain yang boleh hak cipta
+yang kami buat berhubung dengan Perkhidmatan Kami menyediakan. Kami menjamin bahawa Kami mempunyai
+hak undang-undang untuk semua elemen yang berkaitan dengan Perkhidmatan Kami menyediakan dan tidak
+akan memegang Awak bertanggungjawab untuk sebarang tuntutan pihak ketiga.</p>
+
 </li>
 <li class="list-group-item">6. <span class="font-weight-bold">Kerahsiaan.</span>
+
 <p align="justify">
-Rahsia anda selamat dengan Kami. Ini termasuk maklumat proprietari anda (perkara seperti rahsia perdagangan, pengetahuan, atau maklumat rahsia lain yang tidak tersedia secara umum). Kami berjanji Kami tidak akan menjual maklumat proprietari anda kepada pihak ketiga, tidak kira betapa mereka menawarkan Kami dengan harta berbilion atau wanita atau apa-apa keistimewaan.</p>
+Rahsia Awak selamat dengan Kami. Ini termasuk maklumat proprietari Awak (perkara seperti
+rahsia perdagangan, pengetahuan, atau maklumat rahsia lain yang tidak tersedia secara umum). Kami
+berjanji Kami tidak akan menjual maklumat proprietari Awak kepada pihak ketiga, tidak kira
+betapa mereka menawarkan Kami dengan harta berbilion atau wanita atau kuasa atau apa-apa keistimewaan.</p>
 
-<p align="justify">Apa pun Kami akan maklumkan kepada Anda jika ada pihak ketiga merayu meminta data rahsia Anda.</p>
+<p align="justify">
+Apa pun Kami akan maklumkan kepada Awak jika ada pihak ketiga merayu meminta data rahsia Awak.</p>
 
-<p align="justify">Tetapi jika pihak Kerajaan Malaysia yang minta, Kami akan serahkan seperti yang tertakluk dalam undang-undang negara Malaysia tercinta.</p>
+<p align="justify">
+Tetapi jika pihak Kerajaan Malaysia yang minta, Kami akan serahkan seperti yang tertakluk dalam
+undang-undang negara Malaysia tercinta.</p>
 
 </li>
 <li class="list-group-item">7. <span class="font-weight-bold">Tugasan.</span>
+
 <p align="justify">
-Pihak-Pihak tidak boleh menetapkan tanggungjawab yang mereka miliki di bawah Perjanjian ini kepada orang lain melainkan kedua-dua Pihak bersetuju dengan penugasan secara bertulis.</p>
+Pihak-Pihak tidak boleh menetapkan tanggungjawab yang mereka miliki di bawah Perjanjian ini kepada
+orang lain melainkan kedua-dua Pihak bersetuju dengan penugasan secara bertulis.</p>
 </li>
 <li class="list-group-item">8. <span class="font-weight-bold">Penamatan.</span>
+
 <p align="justify">
-Kami sangat tidak suka melihat Engkau pergi. Jika Anda memutuskan Kami bukan cawan kopi (atau wain), Anda boleh mengakhiri Perjanjian ini dengan memberikan Kami notis bertulis sepuluh (10) hari dan membayar Kami untuk Perkhidmatan yang Kami telah selesai.</p>
+Kami sangat tidak suka melihat Engkau pergi. Jika Awak memutuskan Kami bukan cawan kopi
+(atau wain), Awak boleh mengakhiri Perjanjian ini dengan memberikan Kami notis bertulis 
+sepuluh (10) hari dan membayar Kami untuk Perkhidmatan yang Kami telah selesai.</p>
+
 <p align="justify">
-Jika mana-mana Pihak gagal mematuhi tanggungjawab atau kewajipan mereka di bawah Perjanjian ini, Pihak yang satu lagi boleh menamatkan Perjanjian ini dengan memberi sepuluh (10) hari notis bertulis. Perjanjian ini akan ditamatkan secara automatik apabila kedua-dua Pihak telah melaksanakan semua tanggungjawab mereka di bawah Perjanjian dan semua pembayaran telah dibuat.</p>
+Jika mana-mana Pihak gagal mematuhi tanggungjawab atau kewajipan mereka di bawah Perjanjian ini,
+Pihak yang satu lagi boleh menamatkan Perjanjian ini dengan memberi sepuluh (10) hari notis bertulis.
+Perjanjian ini akan ditamatkan secara automatik apabila kedua-dua Pihak telah melaksanakan semua
+tanggungjawab mereka di bawah Perjanjian dan semua pembayaran telah dibuat.</p>
+
 <p align="justify">
-Perjanjian ini akan ditamatkan secara automatik apabila kedua-dua Pihak telah melaksanakan semua tanggungjawab mereka di bawah Perjanjian dan semua pembayaran telah dibuat.</p>
+Perjanjian ini akan ditamatkan secara automatik apabila kedua-dua Pihak telah melaksanakan semua
+tanggungjawab mereka di bawah Perjanjian dan semua pembayaran telah dibuat.</p>
+
 </li>
 <li class="list-group-item">9. <span class="font-weight-bold">Batasan Liabiliti.</span>
+
 <p align="justify">
-Liabiliti anda kepada Kami hanya untuk kos yang perlu dibayar di bawah Perjanjian ini. Anda tidak akan bertanggungjawab kepada Kami, atau mana-mana pihak ketiga, untuk ganti rugi seperti kehilangan keuntungan, kehilangan simpanan, kerosakan sampingan, kerosakan berbangkit, atau ganti rugi khas.</p>
+Liabiliti Awak kepada Kami hanya untuk kos yang perlu dibayar di bawah Perjanjian ini. 
+Awak tidak akan bertanggungjawab kepada Kami, atau mana-mana pihak ketiga, untuk ganti rugi 
+seperti kehilangan keuntungan, kehilangan simpanan, kerosakan sampingan, kerosakan berbangkit,
+atau ganti rugi khas.</p>
+
 </li>
 <li class="list-group-item">10. <span class="font-weight-bold">Resolusi Pertikaian.</span>
+
 	<ol>
-	<li><span class="font-weight-bold">Rundingan:</span>Kami mahu melakukan ini. Sekiranya berlaku pertikaian, pihak-pihak bersetuju untuk bekerja ke arah penyelesaian melalui rundingan yang baik.
+	<li><span class="font-weight-bold">Rundingan:</span>Kami mahu melakukan ini. Sekiranya berlaku
+	pertikaian, pihak-pihak bersetuju untuk bekerja ke arah penyelesaian melalui rundingan yang baik.
 	</li>
-	<li><span class="font-weight-bold">Pengantaraan / Timbang tara:</span>Jika membicarakan perkara itu tidak berjalan dengan lancar, salah satu pihak boleh memulakan pengantaraan atau mengikat timbang tara dalam suatu forum yang dipersetujui oleh Para Pihak
+	<li><span class="font-weight-bold">Pengantaraan / Timbangtara:</span>Jika membicarakan perkara itu
+	tidak berjalan dengan lancar, salah satu pihak boleh memulakan pengantaraan atau mengikat timbangtara
+	dalam suatu forum yang dipersetujui oleh Para Pihak
 	</li>
-	<li><span class="font-weight-bold">Litigasi:</span>Sekiranya litigasi diperlukan, Perjanjian ini akan ditafsirkan berdasarkan undang-undang Negara [Negeri], tanpa mengira apa-apa isu konflik undang-undang yang mungkin timbul. Pihak-pihak bersetuju bahawa pertikaian itu akan diselesaikan di mahkamah yang mempunyai bidang kuasa yang kompeten dalam Negara yang dipersetujui.
+	<li><span class="font-weight-bold">Litigasi:</span>Sekiranya litigasi diperlukan, Perjanjian ini akan
+	ditafsirkan berdasarkan undang-undang Negara [Negeri], tanpa mengira apa-apa isu konflik undang-undang
+	yang mungkin timbul. Pihak-pihak bersetuju bahawa pertikaian itu akan diselesaikan di mahkamah yang
+	mempunyai bidang kuasa yang kompeten dalam Negara yang dipersetujui.
 	</li>
-	<li><span class="font-weight-bold">Yuran Peguam:</span>Pihak yang berlaku, atau "pemenang" sebagai bukan peguam yang memanggilnya, akan dapat memulihkan yuran peguamnya dan lain-lain kos yang munasabah untuk pertikaian yang diselesaikan oleh timbang tara atau litigasi yang mengikat.
+	<li><span class="font-weight-bold">Yuran Peguam:</span>Pihak yang berlaku, atau "pemenang" sebagai
+	bukan peguam yang memanggilnya, akan dapat memulihkan yuran peguamnya dan lain-lain kos yang munasabah
+	untuk pertikaian yang diselesaikan oleh timbang tara atau litigasi yang mengikat.
 	</li>
 	</ol>
 	<!-- ////////////////////////////////////////////////////////////////////////////////////// -->
 </li>
 <li class="list-group-item">11. <span class="font-weight-bold">Kemerosotan.</span>
+
 <p align="justify">
-Sekiranya mana-mana bahagian Perjanjian ini didapati tidak sah, menyalahi undang-undang, atau tidak boleh dikuatkuasakan, seluruh Perjanjian ini masih boleh dikuatkuasakan.</p>
+Sekiranya mana-mana bahagian Perjanjian ini didapati tidak sah, menyalahi undang-undang, atau tidak boleh
+dikuatkuasakan, seluruh Perjanjian ini masih boleh dikuatkuasakan.</p>
 </li>
 <li class="list-group-item">12. <span class="font-weight-bold">Kontrak yang lengkap.</span>
+
 <p align="justify">
-Perjanjian ini meletakkan para Pihak keseluruhan pemahaman tentang Perkhidmatan yang akan dilaksanakan dan apa pun yang dipersetujui Pihak-Pihak dalam hitam dan putih (secara literal). Perjanjian ini menggantikan sebarang komunikasi bertulis atau lisan yang lain antara Pihak-Pihak. Apa-apa perubahan selanjutnya kepada Perjanjian ini mesti dibuat secara bertulis dan ditandatangani oleh kedua-dua Pihak.
+Perjanjian ini meletakkan para Pihak keseluruhan pemahaman tentang Perkhidmatan yang akan dilaksanakan
+dan apa pun yang dipersetujui Pihak-Pihak dalam hitam dan putih (secara literal). Perjanjian ini
+menggantikan sebarang komunikasi bertulis atau lisan yang lain antara Pihak-Pihak. Apa-apa perubahan
+selanjutnya kepada Perjanjian ini mesti dibuat secara bertulis dan ditandatangani oleh kedua-dua Pihak.
 </p>
+
 </li>
 <li class="list-group-item">13. <span class="font-weight-bold">Notis.</span>
+
 <p align="justify">
-Semua notis di bawah perjanjian ini mesti dihantar dengan sama ada e-mel dengan pengesahan penerimaan resit, atau surat siput yang disahkan atau berdaftar dengan resit pulangan yang diminta.</p>
+Semua notis di bawah perjanjian ini mesti dihantar dengan sama ada e-mel dengan pengesahan penerimaan
+resit, atau surat siput yang disahkan atau berdaftar dengan resit pulangan yang diminta.</p>
 
 <p>Notis perlu dihantar kepada:</p>
 
@@ -207,7 +260,8 @@ Semua notis di bawah perjanjian ini mesti dihantar dengan sama ada e-mel dengan 
 </li>
 <li class="list-group-item">14. <span class="font-weight-bold">Mari Berjabat Tangan.</span>
 <p align="justify">
-Ink adalah jabat tangan rasmi perniagaan. Jika Anda bersetuju dengan terma Perjanjian ini, sila tandatangan di bawah. Perjanjian ini akan menjadi berkesan pada [Bulan DD, 20YY].</p>
+Ink adalah jabat tangan rasmi perniagaan. Jika Awak bersetuju dengan terma Perjanjian ini,
+sila tandatangan di bawah. Perjanjian ini akan menjadi berkesan pada [Bulan DD, 20YY].</p>
 
 	<table class="table table-borderless table-sm">
 	<tr><td colspan="2">[Nama Pelanggan]</td><td colspan="2">[Nama syarikat]</td></tr>
@@ -225,11 +279,11 @@ Ink adalah jabat tangan rasmi perniagaan. Jika Anda bersetuju dengan terma Perja
 	</div><!-- / class="card" -->
 	<div class="card">
 		<div class="card-header">
-			<a class="collapsed card-link" data-toggle="collapse" href="#collapseThree">
+			<a class="collapsed card-link" data-toggle="collapse" href="#no03">
 				Isi Kandungan Penjanjian Berdarah Amin Ledang Nombor 2
 			</a>
 		</div><!-- / class="card-header" -->
-		<div id="collapseThree" class="collapse" data-parent="#accordion">
+		<div id="no03" class="collapse" data-parent="#accordion">
 		<div class="card-body">
 <!-- ////////////////////////////////////////////////////////////////////////////////////// -->
 <p align="justify"> Ia bergantung anda berasal dari organisasi yang mana satu.
