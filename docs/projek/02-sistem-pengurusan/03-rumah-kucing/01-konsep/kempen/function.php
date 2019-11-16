527e9ab9583fe99cd5743ec6f8a8ba78e3f78407
Amin Ledang @ Dell <helmi007@yahoo.com>
2019-11-16 14:58:28 +0800

    tambah fungsi baru

--- docs/projek/02-sistem-pengurusan/03-rumah-kucing/01-konsep/kempen/function.php
+++ docs/projek/02-sistem-pengurusan/03-rumah-kucing/01-konsep/kempen/function.php
@@ -1,3 +1,80 @@
 <?php
 #------------------------------------------------------------------------------------------
+function senaraiKempen()
+{
+	$data = array(
+		array(
+			'tajuk'=>'Kucing 1',
+			'gambar'=>'gambar kucing 1',
+			'kempen_target'=>'10000',
+			'kempen_capai'=>'500',
+			'kempen_bil'=>'5',
+			),
+		array(
+			'tajuk'=>'Kucing 2',
+			'gambar'=>'gambar kucing 2',
+			'kempen_target'=>'10000',
+			'kempen_capai'=>'500',
+			'kempen_bil'=>'5',
+			),
+		array(
+			'tajuk'=>'Kucing 1',
+			'gambar'=>'gambar kucing 1',
+			'kempen_target'=>'10000',
+			'kempen_capai'=>'500',
+			'kempen_bil'=>'5',
+			),
+		array(
+			'tajuk'=>'Kucing 1',
+			'gambar'=>'gambar kucing 1',
+			'kempen_target'=>'10000',
+			'kempen_capai'=>'500',
+			'kempen_bil'=>'5',
+			),
+		array(
+			'tajuk'=>'Kucing 1',
+			'gambar'=>'gambar kucing 1',
+			'kempen_target'=>'10000',
+			'kempen_capai'=>'500',
+			'kempen_bil'=>'5',
+			),
+		array(
+			'tajuk'=>'Kucing 1',
+			'gambar'=>'gambar kucing 1',
+			'kempen_target'=>'10000',
+			'kempen_capai'=>'500',
+			'kempen_bil'=>'5',
+			),
+		array(
+			'tajuk'=>'Kucing 1',
+			'gambar'=>'gambar kucing 1',
+			'kempen_target'=>'10000',
+			'kempen_capai'=>'500',
+			'kempen_bil'=>'5',
+			),
+		array(
+			'tajuk'=>'Kucing 1',
+			'gambar'=>'gambar kucing 1',
+			'kempen_target'=>'10000',
+			'kempen_capai'=>'500',
+			'kempen_bil'=>'5',
+			),
+		array(
+			'tajuk'=>'Kucing 1',
+			'gambar'=>'gambar kucing 1',
+			'kempen_target'=>'10000',
+			'kempen_capai'=>'500',
+			'kempen_bil'=>'5',
+			),
+		array(
+			'tajuk'=>'Kucing 1',
+			'gambar'=>'gambar kucing 1',
+			'kempen_target'=>'10000',
+			'kempen_capai'=>'500',
+			'kempen_bil'=>'5',
+			),
+	);
+	#
+	return $data;
+}
 #------------------------------------------------------------------------------------------
