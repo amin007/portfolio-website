83d0951da01f9311743b0716fc64edee8a3df099
Amin Ledang @ Dell <helmi007@yahoo.com>
2019-11-16 17:46:04 +0800

    ringkaskan keterangan

--- docs/projek/02-sistem-pengurusan/03-rumah-kucing/01-konsep/kempen/index.php
+++ docs/projek/02-sistem-pengurusan/03-rumah-kucing/01-konsep/kempen/index.php
@@ -20,10 +20,7 @@ diatas($tajuk);
 	<div class="col-xl-8 py-3">
 		<h1 class="display-3 mb-0"><?php echo $tajuk ?></h1>
 		<p class="lead text-justify">
-		A collection of free, <a href="https://github.com/ThemesGuide/bootstrap-themes" target="_new">open-source</a> themes to give your Bootstrap 4 projects a 
-		<strong>unique, custom</strong> style. Simply download the <code>theme.css</code> file for any theme, and include it after the standard
-		<code>bootstrap.css</code> in your project's HTML templates. Each theme also includes
-		example layout templates to demonstrate Bootstrap 4 elements, components, and content integration.
+		Berikut adalah kempen derma untuk kucing.
 		</p>
 	</div>
 </div>
