ed9ef69e8336a9f53c0f38647f6dbc7bb5d93be0
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-11-24 13:07:58 +0800

    tambah fail baru

---
+++ docs/projek/02-sistem-pengurusan/03-rumah-kucing/00-konsep/upload-file-secure.php
@@ -0,0 +1,32 @@
+<?php
+/*
+#--------------------------------------------------------------------------------------------------
+# https://stackoverflow.com/questions/10456113/php-check-file-extension-in-upload-form
+$allowed =  array('gif','png' ,'jpg');
+$filename = $_FILES['video_file']['name'];
+$ext = pathinfo($filename, PATHINFO_EXTENSION);
+if(!in_array($ext,$allowed) )
+{
+	echo 'error';
+}
+#--------------------------------------------------------------------------------------------------
+https://phppot.com/php/php-image-upload-with-size-type-dimension-validation/
+https://www.daniweb.com/programming/web-development/threads/186170/php-file-upload-validation-before-the-file-uploads
+https://www.php.net/manual/en/features.file-upload.php
+https://www.saotn.org/validate-mime-types-with-php-fileinfo/
+https://www.sitepoint.com/community/t/multiple-upload-file-type-validation/26196
+https://www.w3schools.com/php/func_filesystem_filetype.asp
+###
+https://stackoverflow.com/questions/38509334/full-secure-image-upload-script
+https://github.com/bjorno43/ImageSecure
+https://www.owasp.org/index.php/Unrestricted_File_Upload
+http://webcheatsheet.com/PHP/file_upload.php
+https://www.sitepoint.com/community/t/very-secure-upload-form/258556/5
+https://www.hungred.com/useful-information/secure-file-upload-check-list-php/
+*/
+#--------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
