f49b5a5eef39a1d50eacd994d06d5452eccab2e8
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-11-27 15:32:57 +0800

    tukar text kepada textarea untuk alamat

--- docs/projek/02-sistem-pengurusan/03-rumah-kucing/00-konsep/people/orang/02-penyemak/01-tukang-lapor.php
+++ docs/projek/02-sistem-pengurusan/03-rumah-kucing/00-konsep/people/orang/02-penyemak/01-tukang-lapor.php
@@ -39,7 +39,7 @@
 	<div class="col-lg-2"><span class="input-group-text">alamat rumah</span></div>
 	<div class="col-sm-6">
 		<div class="input-group input-group-lg">
-			<input type="text" class="form-control" placeholder="email anda">
+			<textarea class="form-control" placeholder="alamat anda" rows="3"></textarea>
 		</div><!-- / class="input-group input-group-lg" -->
 	</div><!-- / class="col-sm-6" -->
 </div><!-- / class="form-group row" -->
