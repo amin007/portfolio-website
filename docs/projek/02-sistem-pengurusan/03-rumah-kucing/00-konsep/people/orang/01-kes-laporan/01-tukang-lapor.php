c66c3e08d3c6ca62d562fb4b9a3087c404b53aeb
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-11-27 12:13:56 +0800

    tukar interface

--- docs/projek/02-sistem-pengurusan/03-rumah-kucing/00-konsep/people/orang/01-kes-laporan/01-tukang-lapor.php
+++ docs/projek/02-sistem-pengurusan/03-rumah-kucing/00-konsep/people/orang/01-kes-laporan/01-tukang-lapor.php
@@ -1,71 +1,53 @@
 <!-- sempadan input tengah  +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ -->
 <div class="form-group row">
-	<!-- div class="col-2"></div -->
+	<div class="col-lg-2"><span class="input-group-text">Nama penuh</span></div>
 	<div class="col-sm-6">
 		<div class="input-group input-group-lg">
-			<span class="input-group-prepend"><span class="input-group-text">
-				nama penuh&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-			</span></span>
 			<input type="text" class="form-control" placeholder="nama anda">
 		</div>
 	</div>
 </div>
 <!-- sempadan input tengah  +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ -->
 <div class="form-group row">
-	<!-- div class="col-2"></div -->
+	<div class="col-lg-2"><span class="input-group-text">No KP</span></div>
 	<div class="col-sm-6">
 		<div class="input-group input-group-lg">
-			<span class="input-group-prepend"><span class="input-group-text">
-				no kp&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-			</span></span>
 			<input type="text" class="form-control" placeholder="nama anda">
 		</div>
 	</div>
 </div>
 <!-- sempadan input tengah  +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ -->
 <div class="form-group row">
-	<!-- div class="col-2"></div -->
+	<div class="col-lg-2"><span class="input-group-text">nombor telefon</span></div>
 	<div class="col-sm-6">
 		<div class="input-group input-group-lg">
-			<span class="input-group-prepend"><span class="input-group-text">
-				nombor telefon
-			</span></span>
 			<input type="text" class="form-control" placeholder="no tel anda">
 		</div>
 	</div>
 </div>
 <!-- sempadan input tengah  +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ -->
 <div class="form-group row">
-	<!-- div class="col-2"></div -->
+	<div class="col-lg-2"><span class="input-group-text">email anda</span></div>
 	<div class="col-sm-6">
 		<div class="input-group input-group-lg">
-			<span class="input-group-prepend"><span class="input-group-text">
-				email anda&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-			</span></span>
 			<input type="text" class="form-control" placeholder="email anda">
 		</div>
 	</div>
 </div>
 <!-- sempadan input tengah  +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ -->
 <div class="form-group row">
-	<!-- div class="col-2"></div -->
+	<div class="col-lg-2"><span class="input-group-text">alamat rumah</span></div>
 	<div class="col-sm-6">
 		<div class="input-group input-group-lg">
-			<span class="input-group-prepend"><span class="input-group-text">
-				alamat rumah&nbsp;&nbsp;&nbsp;
-			</span></span>
 			<input type="text" class="form-control" placeholder="email anda">
 		</div>
 	</div>
 </div>
 <!-- sempadan input tengah  +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ -->
 <div class="form-group row">
-	<!-- div class="col-2"></div -->
+	<div class="col-lg-2"><span class="input-group-text">tarikh</span></div>
 	<div class="col-sm-6">
 		<div class="input-group input-group-sm">
-			<span class="input-group-prepend"><span class="input-group-text">
-				tarikh
-			</span></span>
 			<input type="date" class="form-control date-picker"
 			placeholder="Cth: 2014-05-01" id="date" data-date-format="yyyy/mm/dd" />
 		</div>
@@ -73,13 +55,10 @@
 </div>
 <!-- sempadan input tengah  +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ -->
 <div class="form-group row">
-	<!-- div class="col-2"></div -->
+	<div class="col-lg-2"><span class="input-group-text">Gambar</span></div>
 	<div class="col-sm-6">
 		<div class="input-group input-group-lg">
-			<span class="input-group-prepend"><span class="input-group-text">
-				Gambar
-			</span></span>
-			<input type="file" class="form-control" id="exampleFormControlFile1">
+			<input type="file" class="form-control">
 			<br>
 		</div>
 	</div>
