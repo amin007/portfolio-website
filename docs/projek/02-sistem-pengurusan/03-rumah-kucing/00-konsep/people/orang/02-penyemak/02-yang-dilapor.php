c961bb5829f724942ca7cca5d6d6bbb4e1496454
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-11-27 15:33:42 +0800

    tukar kepada no kp anda

--- docs/projek/02-sistem-pengurusan/03-rumah-kucing/00-konsep/people/orang/02-penyemak/02-yang-dilapor.php
+++ docs/projek/02-sistem-pengurusan/03-rumah-kucing/00-konsep/people/orang/02-penyemak/02-yang-dilapor.php
@@ -12,7 +12,7 @@
 	<div class="col-lg-2"><span class="input-group-text">No KP</span></div>
 	<div class="col-sm-6">
 		<div class="input-group input-group-lg">
-			<input type="text" class="form-control" placeholder="nama anda">
+			<input type="text" class="form-control" placeholder="no kp anda">
 		</div><!-- / class="input-group input-group-lg" -->
 	</div><!-- / class="col-sm-6" -->
 </div><!-- / class="form-group row" -->
