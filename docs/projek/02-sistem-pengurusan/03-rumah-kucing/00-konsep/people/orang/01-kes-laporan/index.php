c9e82eec53d58bacf1713b329fb493d8a70e72c2
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-11-27 10:55:17 +0800

    fail utama

---
+++ docs/projek/02-sistem-pengurusan/03-rumah-kucing/00-konsep/people/orang/01-kes-laporan/index.php
@@ -0,0 +1,34 @@
+<?php
+include '../../../main-function.php';
+//include 'function.php';
+$tajuk = 'Kempen Derma';
+diatas($tajuk);
+?>
+
+<!-- untuk menubar atas
+=============================================================================================== -->
+<?php navbar(); ?>
+<!-- ==============================================================================================
+-->
+
+<!-- untuk konten tengah
+=============================================================================================== -->
+<form method="POST" action="" class="form-horizontal"><!-- style="background-color: #e9ecef;" -->
+<!-- mula - borang utama ---------------------------------------------------------------------------------------------- -->
+<?php
+/*include 'x-contoh-nav-tabs-01.php'; echo '<hr>';
+include 'x-contoh-nav-tabs-02.php'; echo '<hr>';
+include 'x-contoh-nav-tabs-03.php'; echo '<hr>';
+include 'x-contoh-nav-tabs-04.php'; echo '<hr>';//*/
+include 'kes-laporan.php'; echo '<hr>';
+?>
+<!-- tamat - borang utama ---------------------------------------------------------------------------------------------- -->
+</form>
+<!-- ==============================================================================================
+tamat untuk konten tengah -->
+
+<!-- untuk menubar bawah
+=============================================================================================== -->
+<?php
+notakaki();
+dibawah();
