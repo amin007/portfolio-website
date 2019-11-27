3711e5347f20fd5e98c29e08688a0cad59d3cc1f
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-11-27 15:06:49 +0800

    tambah nota tambahan

--- docs/projek/02-sistem-pengurusan/03-rumah-kucing/00-konsep/people/orang/01-kes-laporan/02-yang-dilapor.php
+++ docs/projek/02-sistem-pengurusan/03-rumah-kucing/00-konsep/people/orang/01-kes-laporan/02-yang-dilapor.php
@@ -4,45 +4,45 @@
 	<div class="col-sm-6">
 		<div class="input-group input-group-lg">
 			<input type="text" class="form-control" placeholder="nama anda">
-		</div>
-	</div>
-</div>
+		</div><!-- / class="input-group input-group-lg" -->
+	</div><!-- / class="col-sm-6" -->
+</div><!-- / class="form-group row" -->
 <!-- sempadan input tengah  +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ -->
 <div class="form-group row">
 	<div class="col-lg-2"><span class="input-group-text">No KP</span></div>
 	<div class="col-sm-6">
 		<div class="input-group input-group-lg">
 			<input type="text" class="form-control" placeholder="nama anda">
-		</div>
-	</div>
-</div>
+		</div><!-- / class="input-group input-group-lg" -->
+	</div><!-- / class="col-sm-6" -->
+</div><!-- / class="form-group row" -->
 <!-- sempadan input tengah  +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ -->
 <div class="form-group row">
 	<div class="col-lg-2"><span class="input-group-text">nombor telefon</span></div>
 	<div class="col-sm-6">
 		<div class="input-group input-group-lg">
 			<input type="text" class="form-control" placeholder="no tel anda">
-		</div>
-	</div>
-</div>
+		</div><!-- / class="input-group input-group-lg" -->
+	</div><!-- / class="col-sm-6" -->
+</div><!-- / class="form-group row" -->
 <!-- sempadan input tengah  +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ -->
 <div class="form-group row">
 	<div class="col-lg-2"><span class="input-group-text">email anda</span></div>
 	<div class="col-sm-6">
 		<div class="input-group input-group-lg">
 			<input type="text" class="form-control" placeholder="email anda">
-		</div>
-	</div>
-</div>
+		</div><!-- / class="input-group input-group-lg" -->
+	</div><!-- / class="col-sm-6" -->
+</div><!-- / class="form-group row" -->
 <!-- sempadan input tengah  +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ -->
 <div class="form-group row">
 	<div class="col-lg-2"><span class="input-group-text">alamat rumah</span></div>
 	<div class="col-sm-6">
 		<div class="input-group input-group-lg">
 			<input type="text" class="form-control" placeholder="email anda">
-		</div>
-	</div>
-</div>
+		</div><!-- / class="input-group input-group-lg" -->
+	</div><!-- / class="col-sm-6" -->
+</div><!-- / class="form-group row" -->
 <!-- sempadan input tengah  +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ -->
 <div class="form-group row">
 	<div class="col-lg-2"><span class="input-group-text">tarikh</span></div>
@@ -50,9 +50,9 @@
 		<div class="input-group input-group-sm">
 			<input type="date" class="form-control date-picker"
 			placeholder="Cth: 2014-05-01" id="date" data-date-format="yyyy/mm/dd" />
-		</div>
-	</div>
-</div>
+		</div><!-- / class="input-group input-group-lg" -->
+	</div><!-- / class="col-sm-6" -->
+</div><!-- / class="form-group row" -->
 <!-- sempadan input tengah  +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ -->
 <div class="form-group row">
 	<div class="col-lg-2"><span class="input-group-text">Gambar</span></div>
@@ -60,14 +60,14 @@
 		<div class="input-group input-group-lg">
 			<input type="file" class="form-control">
 			<br>
-		</div>
-	</div>
-</div>
+		</div><!-- / class="input-group input-group-lg" -->
+	</div><!-- / class="col-sm-6" -->
+</div><!-- / class="form-group row" -->
 <!-- sempadan input tengah  +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ -->
 <div class="form-group row">
 	<!-- div class="col-2"></div -->
 	<div class="col-sm-6">
 		<p class="form-control-static text-info">{contoh-data}</p>
-	</div>
-</div>
+	</div><!-- / class="col-sm-6" -->
+</div><!-- / class="form-group row" -->
 <!-- sempadan input tengah  +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ -->
