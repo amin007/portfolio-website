6d367b0c186b51de024639dd82a7c31c5fee7c84
Amin Ledang @ Dell <helmi007@yahoo.com>
2019-12-07 17:12:10 +0800

    buat contoh projek

---
+++ docs/projek/00-sistem-error/02-iklan-kecantikan/index.php
@@ -0,0 +1,27 @@
+<?php
+require 'main-function.php';
+diatas();
+diheadbody()
+?>
+
+<br><br><br>
+<!-- ////////////////////////////////////////////////////////////////////////////////////////// -->
+<dl class="row">
+	<dt class="col-12 bg-success text-center"><img width="50%" src="https://cdn.pixabay.com/photo/2017/09/07/19/38/soap-2726371_960_720.jpg"></dt>
+</dl>
+<div class="container">
+<dl class="row">
+	<dt class="col-1"></dt>
+	<dt class="col-3">Coffee</dt>
+	<dd class="col-8">Black hot drink</dd>
+	<dt class="col-4">Milk</dt>
+	<dd class="col-8">White cold drink</dd>
+</dl>
+</div><!-- / class="container" -->
+<!-- ////////////////////////////////////////////////////////////////////////////////////////// -->
+
+<?php
+dibawah();
+// jquery di sini
+dibodyhtml();
+?>
