a653fcd90e3db28856e6b710646d50df03fd59d6
Amin Ledang @ Dell <helmi007@yahoo.com>
2019-12-07 18:10:14 +0800

    tambah gambar baru

--- docs/projek/00-sistem-error/02-iklan-kecantikan/index.php
+++ docs/projek/00-sistem-error/02-iklan-kecantikan/index.php
@@ -8,11 +8,13 @@ $data0 = $data[0];
 $data1 = $data[1];
 $data2 = $data[2];
 ?>
-
-<br><br><br>
+<br>
 <!-- ////////////////////////////////////////////////////////////////////////////////////////// -->
-<div class="row">
-	<div class="col-12 bg-success text-center"><img width="50%" src="https://cdn.pixabay.com/photo/2017/09/07/19/38/soap-2726371_960_720.jpg">
+<!--
+https://pixabay.com/photos/soap-horsetail-salt-blossom-bloom-2333412/
+-->
+<div class="row" style="background:#F4F9FD">
+	<div class="col-12 text-center"><img src="images/soap-2333412_640.jpg">
 	</div><!-- / class="col-12" -->
 </div><!-- / class="row" -->
 <!-- ////////////////////////////////////////////////////////////////////////////////////////// -->
@@ -53,6 +55,10 @@ $data2 = $data[2];
 </div><!-- / class="row" -->
 <!-- ////////////////////////////////////////////////////////////////////////////////////////// -->
 
+<br>
+<footer class="text-center">
+Image by <a href="https://pixabay.com/users/silviarita-3142410/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=2333412">silviarita</a> from <a href="https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=2333412">Pixabay</a>
+</footer>
 <?php
 dibawah();
 // jquery di sini
