8483549fe01d5c0452f89496f2e37ae349613f70
Amin Ledang @ Dell <helmi007@yahoo.com>
2019-12-07 16:05:25 +0800

    buat projek baru

---
+++ docs/projek/00-sistem-error/02-iklan-kecantikan/main-function.php
@@ -0,0 +1,25 @@
+<?php
+#--------------------------------------------------------------------------------------------------
+/*
+https://cdn.pixabay.com/photo/2015/08/25/03/51/toner-906142_960_720.jpg
+https://cdn.pixabay.com/photo/2017/09/07/19/43/soap-2726387_960_720.jpg
+https://cdn.pixabay.com/photo/2017/09/07/19/46/soap-2726394_960_720.jpg
+https://images.unsplash.com/photo-1556217257-aa1d0c385e62?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80
+*/
+#--------------------------------------------------------------------------------------------------
+function content()
+{
+	$data[] = 'pepsi';
+
+	$data[] = 'sosial media posts';
+
+	$data[] = 'Pepsi in Pakistan is very versatile when it come to sosial media posting. There are
+several pillors under which they work. Most of them are engagamennd related like
+music, cricket, refreashment posts. Also they are main sponser of bigger music
+platform Pakistan Idol. Showcasing some of Pepsi posts. 
+';
+
+	return $data;
+}
+#--------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
