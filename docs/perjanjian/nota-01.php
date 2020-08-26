6ea7cee61cf2f2db40e01ca9dae9f276d8a67990
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2020-08-26 16:41:31 +0800

    tambah nota tambahan

---
+++ docs/perjanjian/nota-01.php
@@ -0,0 +1,5 @@
+https://global-sharepoint.com/sharepoint-2/o365-getting-started-with-microsoft-teams-in-office-365/
+https://global-sharepoint.com/sharepoint-2/create-teams-in-microsoft-teams-step-by-step-process-o365/
+https://global-sharepoint.com/sharepoint-2/office-365-sharepoint-vs-microsoft-teams-understanding-the-differences-in-o365/
+https://www.microsoft.com/en-my/microsoft-365/microsoft-teams/free?rtc=1
+https://www.microsoft.com/en-my/microsoft-365/microsoft-teams/group-chat-software
