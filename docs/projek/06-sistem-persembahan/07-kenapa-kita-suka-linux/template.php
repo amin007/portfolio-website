6c2aab9617816fe2f932c77ba631cb7c5b2e7908
Amin Di PC HP Elite Tower 800 G9 <al.amin007@yahoo.com>
2025-07-28 15:21:08 +0800

    buat template khas

---
+++ docs/projek/06-sistem-persembahan/07-kenapa-kita-suka-linux/template.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Pembentangan Pertama Saya</title>
+<link rel="stylesheet" href="">
+</head>
+<body>
+
+<!-- khas untuk article
+=============================================================================================== -->
+<article>
+	<h1>Persembahan Pertama</h1>
+</article>
+
+<article>
+	<h1>Persembahan Kedua</h1>
+</article>
+
+<!-- khas untuk jquery dan js2 lain
+=============================================================================================== -->
+<script src=""></script>
+
+<!-- bawah kaki
+=============================================================================================== -->
+</body>
+</html>
